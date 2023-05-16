f72a8dbec14f7d09156ab010664f0d6b5524ce32
gaoshuaixing <gaoshuaixing@bilibili.com>
2023-05-16 19:28:12 +0800

    d

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,23 @@
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '文档', link: '/pages/8ef798/', 
+    items: [
+      { text: 'v3.x', link: '/pages/8ef798/' },
+      { text: 'v2.x', link: '/pages/96f672/' },
+    ]
+  },
+  { text: 'API', link: '/pages/85c531/' },
+  { text: '功能', link: '/pages/132909/' },
+  { text: '支持与赞助', link: '/pages/cce31f/' },
+  { text: '知识点', link: '/pages/1f0f51/' },
+  { text: '案例', link: '/pages/eadf46/' },
+  { text: '交流', link: '/pages/c2720e/' },
+  {
+    text: '开源', link: '#', 
+    items: [
+      { text: 'GitHub', link: 'https://github.com/dromara/electron-egg' },
+      { text: 'Gitee', link: 'https://gitee.com/dromara/electron-egg' },
+    ]
+  },
+];
