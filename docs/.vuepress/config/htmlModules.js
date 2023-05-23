823989342b402aff79e6dcd4749edd3fe496dcb2
gsx <530353222@qq.com>
2023-05-24 06:40:21 +0800

    ad

--- docs/.vuepress/config/htmlModules.js
+++ docs/.vuepress/config/htmlModules.js
@@ -58,7 +58,15 @@ module.exports = {
   `,
   //
   pageB: `
-
+    <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-2708221248260848"
+      data-ad-slot="5098589217"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
   `,
   windowRB: `
 
