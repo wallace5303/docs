017e4b86fc5e7acab7d0b33cde9457af0a5f3a81
gaoshuaixing <gaoshuaixing@bilibili.com>
2023-06-12 16:24:20 +0800

    支持与赞助

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -9,7 +9,7 @@ module.exports = [
   },
   { text: 'API', link: '/pages/85c531/' },
   { text: '功能', link: '/pages/132909/' },
-  { text: '支持与赞助', link: '/pages/cce31f/' },
+  { text: '支持与赞助', link: '/pages/9365a8/' },
   { text: '知识点', link: '/pages/1f0f51/' },
   { text: '案例', link: '/pages/eadf46/' },
   { text: '交流', link: '/pages/c2720e/' },
