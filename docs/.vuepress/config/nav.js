99a812dfe64005a4dfb4bf2274ae56325c788e06
gsx <530353222@qq.com>
2023-07-17 22:58:35 +0800

    支持

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -8,7 +8,7 @@ module.exports = [
     ]
   },
   { text: 'API', link: '/pages/85c531/' },
-  { text: '功能', link: '/pages/132909/' },
+  { text: 'demo', link: '/pages/132909/' },
   { text: '支持与赞助', link: '/pages/9365a8/' },
   { text: '知识点', link: '/pages/1f0f51/' },
   { text: '案例', link: '/pages/eadf46/' },
